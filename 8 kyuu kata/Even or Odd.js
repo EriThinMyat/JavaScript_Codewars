278b3395c30015ea8fe6c13c2fd1d639025b950d
Aye Thin Myat <102400616+EriThinMyat@users.noreply.github.com>
2022-05-12 18:36:21 -0700

    Create Even or Odd.js

---
+++ 8 kyuu kata/Even or Odd.js
@@ -0,0 +1,5 @@
+/* Description
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
+
+function evenOdd(n){
+  return n % 2 == 0 ? 'Even' : 'Odd';
